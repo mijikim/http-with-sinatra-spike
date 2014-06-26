42422f80e9108da77c86bd406c5af2cbcb63d52e
mijikim <miji.kim926@gmail.com>
2014-06-26 16:28:32 -0600

    App rb to push to Heroku

--- app.rb
+++ app.rb
@@ -1,17 +1,33 @@
 require "sinatra/base"
-require "sinatra/reloader"
 
 class MyApp < Sinatra::Application
-  register Sinatra::Reloader
 
   def initialize
     super
-    @items = []
+    @items = ["ice cream", "milkshake", "pie"]
   end
 
   get "/" do
     "items length: #{@items.length}"
+    erb :root
+  end
+
+  get "/items" do
+    filter = params[:filter]
+    @filtered_items = @items
+    @filtered_items = @items.select {|x| x.include?(filter)} unless filter == nil
+    erb :items
+  end
+
+  get "/items/new" do
+    erb :new
+  end
+
+  get "/items/:id" do
+    id = params[:id].to_i
+    "You found item: #{@items[id]}"
   end
 
   run! if app_file == $0
+
 end
